06c2c5bc9c793803a64658469bcb09f3e47b0c56
Shriya-Sonam <86159436+Shriya-Sonam@users.noreply.github.com>
2023-07-06 00:40:02 +0530

    Time: 5 ms (13.32%), Space: 41.2 MB (12.55%) - LeetHub

--- 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.java
+++ 2605-form-smallest-number-from-two-digit-arrays/2605-form-smallest-number-from-two-digit-arrays.java
@@ -1,19 +1,32 @@
-class Solution {
-    public int minNumber(int[] nums1, int[] nums2) {
+class Solution
+{
+    public int minNumber(int[] nums1, int[] nums2) 
+    {
         Set<Integer> s1 = getHashSet(nums1), s2 = getHashSet(nums2);
+        
         s1.retainAll(s2);
-        if (!s1.isEmpty()) {
+        
+        if (!s1.isEmpty()) 
+        {
             return s1.stream().mapToInt(i -> i).min().getAsInt();
         }
+        
         int a = IntStream.of(nums1).min().getAsInt();
+        
         int b = IntStream.of(nums2).min().getAsInt();
+        
         return Math.min(a, b) * 10 + Math.max(a, b);
     }
-    private HashSet<Integer> getHashSet(int[] nums) {
+    private HashSet<Integer> getHashSet(int[] nums) 
+    {
         HashSet<Integer> set = new HashSet<>();
-        for (int num : nums) {
+        
+        for (int num : nums) 
+            
+        {
             set.add(num);
         }
+        
         return set;
         
     }
