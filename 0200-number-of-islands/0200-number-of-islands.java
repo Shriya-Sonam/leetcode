db9f7345096870cdac017c5976d6a6f225488770
Shriya-Sonam <86159436+Shriya-Sonam@users.noreply.github.com>
2023-07-22 23:41:20 +0530

    Time: 4 ms (53.21%), Space: 47.9 MB (36.02%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -11,7 +11,7 @@ class Solution {
     }
     int sink(int i, int j) {
         if (i < 0 || i == g.length || j < 0 || j == g[i].length || g[i][j] == '0')
-            return 0;
+        return 0;
         g[i][j] = '0';
         sink(i+1, j); sink(i-1, j); sink(i, j+1); sink(i, j-1);
         return 1;
